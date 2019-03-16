1e89dfb8b06f801f1fa1a1be2631c7a170490107
singh-p <priyanka3811053@gmail.com>
2019-03-16 21:24:42 +0530

    my first code

---
+++ functionlist.R
@@ -0,0 +1,11 @@
+
+info <- function(fn,ln,ins){
+op<-paste(fn,' ', ln, ', ', ins, sep='')
+return(op)
+}
+
+info <- function(fn,ln,ins,pn){
+op<-paste(fn,' ',ln, ', ',ins,', ',pn, sep='')
+return(op)
+}
+
